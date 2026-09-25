495d701075aaeb86fd6558b0d3990f5d63e85c22
Gabriel <gabrielsar993@gmail.com>
2026-09-25 18:56:31 +0200

    todo medio establecido

---
+++ 02-RotX/RotX.java
@@ -0,0 +1,23 @@
+public class RotX {
+        static char[] minusculas = {
+        'a', 'á', 'à', 'b', 'c', 'ç', 'd', 'e', 'é', 'è',
+        'f', 'g', 'h', 'i', 'í', 'ì', 'ï', 'j', 'k', 'l',
+        'm', 'n', 'ñ', 'o', 'ó', 'ò', 'p', 'q', 'r', 's',
+        't', 'u', 'ú', 'ù', 'ü', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    static char[] mayusculas = {
+        'A', 'Á', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È',
+        'F', 'G', 'H', 'I', 'Í', 'Ì', 'Ï', 'J', 'K', 'L',
+        'M', 'N', 'Ñ', 'O', 'Ó', 'Ò', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'Ú', 'Ù', 'Ü', 'V', 'W', 'X', 'Y', 'Z'
+    };
+
+
+    //xifratRot hace que se xifren dependiendo de la cadena y la cantidad de saltos
+
+    //desxifratRot hace lo mismo pero hacia atrás
+
+    //forcaBruta le das la cadena cifrada y va desifrando 1 por 1 
+
+}
